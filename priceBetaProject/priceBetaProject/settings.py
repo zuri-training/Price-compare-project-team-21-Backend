a1ff69a9bc7d1783e01ac13c161737f9fef78289
Afeh <balogunafebu22@gmail.com>
2022-08-08 12:53:33 +0100

    Fixed non-displaying index page and added tk to requirements.txt

--- priceBetaProject/priceBetaProject/settings.py
+++ priceBetaProject/priceBetaProject/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     #Created apps
     'priceBetaApp',
     'Accounts',
-    'priceBetaApp',
     
 ]
 
@@ -124,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_ROOT = os.path.join(BASE_DIR, 'static'),
 
 # Default primary key field type
