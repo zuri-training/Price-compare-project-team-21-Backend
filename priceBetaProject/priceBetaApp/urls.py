2780cd56a03bb4b36c22fb33a7b3ce3de85baba1
Afeh <balogunafebu22@gmail.com>
2022-08-08 15:19:54 +0100

    Fixed CSS issue and fixed static settings in settings.py and urls.py

--- priceBetaProject/priceBetaApp/urls.py
+++ priceBetaProject/priceBetaApp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from .views import Product, Category, Wishlist, Store
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -9,4 +11,4 @@ urlpatterns = [
     path('', Category),
     path('', Store),
     path('', Wishlist)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
