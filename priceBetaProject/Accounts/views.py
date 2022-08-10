e7d1c85fb8f3da3f885839627550ce6f68470c1e
Ade-nike17 <105179160+Ade-nike17@users.noreply.github.com>
2022-08-10 11:33:41 +0100

    Revert "integrates frontend into django"

--- priceBetaProject/Accounts/views.py
+++ priceBetaProject/Accounts/views.py
@@ -3,8 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.core.mail import send_mail
 from .forms import CustomUserCreationForm
-from django.shortcuts import render
-from django.contrib.auth import login
+
 
 # Create your views here.
 class SignUpView(CreateView):
@@ -12,5 +11,5 @@ class SignUpView(CreateView):
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
-def login(request):
-    return render(request,'login.html')
+
+
--- priceBetaProject/priceBetaApp/urls.py
+++ priceBetaProject/priceBetaApp/urls.py
@@ -7,14 +7,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 	path('index', views.index, name = "index"),
-    path('about', views.about, name = "about"),
-    path('login', views.login, name = "login"),
-    path('contact', views.contact, name = "contact"),
-    path('privacy', views.privacy, name = "privacy"),
-    path('term', views.term, name = "term"),
-    path('accessories_category', views.accessories_category, name = "accessories_category"),
-    path('laptop_category', views.laptop_category, name = "laptop_category"),
-    path('phone_category', views.phone_category, name = "phone_category"),
 	path('', Product),
     path('', Category),
     path('', Store),
