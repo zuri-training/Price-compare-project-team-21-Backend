458b3d33d5026c6ae34053e29658181954bd72cf
Nyke <perfectnyke@gmail.com>
2022-08-10 11:19:33 +0100

    integrates frontend into django

--- priceBetaProject/priceBetaApp/views.py
+++ priceBetaProject/priceBetaApp/views.py
@@ -9,7 +9,31 @@ from django.shortcuts import render, redirect, HttpResponse
 
 
 def index(request):
-     return render(request,'index.html')
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def privacy(request):
+    return render(request,'privacy.html')
+
+def term(request):
+    return render(request,'term.html')
+
+def accessories_category(request):
+    return render(request,'accessories_category.html')
+
+def laptop_category(request):
+    return render(request,'laptop_category.html')
+
+def phone_category(request):
+    return render(request,'phone_category.html')
 
 def Product(request):
     return HttpResponse("It is working")
