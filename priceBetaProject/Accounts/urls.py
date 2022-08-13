c6114cb060dda4fd13db866b9a67198b86c8a1fd
Nyke <perfectnyke@gmail.com>
2022-08-13 17:12:43 +0100

    welcome email done

--- priceBetaProject/Accounts/urls.py
+++ priceBetaProject/Accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from .views import SignUpView
 from . import views
 
--- priceBetaProject/Accounts/views.py
+++ priceBetaProject/Accounts/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django import http
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -5,6 +7,7 @@ from django.core.mail import send_mail
 #from .forms import CustomUserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login, authenticate
+from django.conf import settings
 
 from .forms import SignUpForm
 
@@ -14,11 +17,25 @@ def SignUpView(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
+            username = request.POST['username']
+            email = request.POST['email']
+            subject = "Welcome to PriceBeta"
+            message = f'Hi {username}, thank you for choosing PriceBeta, we will help you save your money while giving you quality products. At PriceBeta, you will have a wide range of products to compare and choose from.'
+            from_email = settings.EMAIL_HOST_USER
+            recipient_list = [email]
+            send_mail(subject, message, from_email, recipient_list, fail_silently=False)
+           
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password = raw_password)
+            messages.success(request, 'Your account has been created successfully, you can now log in')
+            return render(request, 'registration/signup.html')
+        else:
+            messages.error(request, 'Invalid form submission.')
+            messages.error(request, form.errors)
+            #return http.HttpResponseRedirect('signup')
             auth_login(request, user)
-            return redirect('index')
+        return redirect('login')
 
     else:
         form = SignUpForm()
